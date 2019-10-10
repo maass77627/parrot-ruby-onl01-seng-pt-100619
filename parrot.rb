ed7463d09dce60404cca274decc2fb48aaaef9bf
kelly r maass <kmaass77627@gmail.com>
2019-10-10 22:23:05 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,13 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(name ="Squawk!")
-  puts "#{name}"
-  end
-  
+def parrot(string="Squawk!")
+  puts "#{string}"
+  "#{string}"
+end
 
-parrot
 
-parrot("jon")
 
 
